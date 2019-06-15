28df17ded8dda93a453a58e34be17ec45bb89f98
Marzuk Rashid <marzukr@gmail.com>
2019-06-15 17:41:56 -0500

    working solution?

--- disorderly_escape/solution.py
+++ disorderly_escape/solution.py
@@ -60,13 +60,75 @@
 #     print(solution(1,i,3))
 # print(solution(2,3,2))
 
+from collections import Counter
+from math import factorial, gcd
+
+def lcm(a, b):
+    return abs(a*b) // gcd(a, b)
+
 def partitions(n, I=1):
-    yield (n,)
+    yield Counter({n: 1})
     for i in range(I, n//2 + 1):
         for p in partitions(n-i, i):
-            yield (i,) + p
+            yield Counter({i: 1}) + p
 
-print(list(partitions(6)))
+class CycleTerm:
+    def __init__(self, coefficient, variables):
+        self.coefficient = coefficient
+        self.variables = variables
+
+    def combine(self, term):
+        coefficient = self.coefficient * term.coefficient
+        variables = []
+        for v1 in self.variables:
+            for v2 in term.variables:
+                lcm_calc = lcm(v1.subscript, v2.subscript)
+                superscript = (v1.subscript * v1.superscript * v2.subscript * v2.superscript) / lcm_calc
+                variables.append(Variable(superscript, lcm_calc))
+        return CycleTerm(coefficient, variables)
+
+    def plug_in(self, s):
+        total = 1
+        for var in self.variables:
+            total *= s**var.superscript
+        return total * self.coefficient
+
+class Variable:
+    def __init__(self, superscript, subscript):
+        self.superscript = superscript
+        self.subscript = subscript
+
+def cycle_index(n):
+    terms = []
+    for j in partitions(n):
+        coefficient = 1
+        variables = []
+        for k in range(1, n + 1): 
+            if k in j:
+                coefficient *= k**j[k] * factorial(j[k])
+                variables.append(Variable(j[k], k))
+        coefficient = 1 / coefficient
+        terms.append(CycleTerm(coefficient, variables))
+    return terms
+
+def combine_cycle_index(c1, c2):
+    terms = []
+    for term1 in c1:
+        for term2 in c2:
+            terms.append(term1.combine(term2))
+    return terms
+
+def calc_cycle_index_result(cycle_index, s):
+    result = 0
+    for term in cycle_index:
+        result += term.plug_in(s)
+    return result
 
 
-    
+def solution(w, h, s):
+    row_cycle_index = cycle_index(h)
+    col_cycle_index = cycle_index(w)
+    comb_cycle_index = combine_cycle_index(row_cycle_index, col_cycle_index)
+    return str(int(calc_cycle_index_result(comb_cycle_index, s)))
+
+print(solution(2,3,4))
